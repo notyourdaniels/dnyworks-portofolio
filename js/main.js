699999137cd772209cff4ce7b746fe9fc4aa0f42
notyourdaniels <60432826+notyourdaniels@users.noreply.github.com>
2021-06-22 12:56:27 +0700

    fixing random text bug

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 //shadow header
 window.onscroll = () => {
-  shadowHeader();
-};
-
-const shadowHeader = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("solidHeader").className = "solidHeaderShadow";
   } else {
@@ -57,7 +53,37 @@ const randomText = [
   "🌙 Finally, I can sleep",
   "Continue to fight, continue to win.",
   "Nothing comes from rushing.",
+  "Actually, I'm not a big fan of coffee",
+  "It cost $ 0.00 to be a nice person",
+  "Relax, you got this !",
+  "My first programming language is PASCAL",
+  "Alien speaks Java language",
 ];
 
-const random = Math.floor(Math.random() * randomText.length);
-document.getElementById("random-text").innerHTML = randomText[random];
+let random = 0;
+if (typeof Storage !== "undefined") {
+  if (localStorage.randomFooter) {
+    random = Math.floor(Math.random() * randomText.length);
+    while (random === localStorage.randomFooter) {
+      random = Math.floor(Math.random() * randomText.length);
+    }
+  } else {
+    localStorage.randomFooter = Math.floor(Math.random() * randomText.length);
+  }
+} else {
+  random = Math.floor(Math.random() * randomText.length);
+}
+
+if (document.getElementById("random-text")) {
+  document.getElementById("random-text").innerHTML = randomText[random];
+}
+
+//console log styling
+console.log(
+  "%c HAYO KAMU MAU NGAPAIN ?!",
+  "font-weight: bold; font-size: 50px;color: red"
+);
+console.log(
+  `%c Yahh ketauan dehhh 😔 \n \n \n © dnyworks 2021 \n ${randomText[random]}`,
+  "font-size: 20px; margin-left: 15px"
+);
